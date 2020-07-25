739906010784265adc2039639983978b221299b6
kento <kozukata1993@gmail.com>
2020-07-25 14:43:28 +0900

    firestore導入 firestoreへユーザーからのアクセスを制限

--- functions/src/createUser.ts
+++ functions/src/createUser.ts
@@ -4,6 +4,12 @@ import * as admin from 'firebase-admin';
 
 export const createUser = async (req: functions.https.Request, res: functions.Response) => {
   const events = req.body.events as line.FollowEvent[];
-  await admin.firestore().collection('users').add({ uid: events[0].source.userId });
+  await admin
+    .firestore()
+    .collection('users')
+    .add({ uid: events[0].source.userId, createdAt: admin.firestore.FieldValue.serverTimestamp() })
+    .catch((error) => {
+      console.log(error);
+    });
   res.status(200).end();
 };
